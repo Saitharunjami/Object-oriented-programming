1d347fb8a6bbceef779a4128c5d243d249ae8f2e
Jami Sai Tharun <98573953+Saitharunjami@users.noreply.github.com>
2022-04-01 15:05:10 +0530

    Create generic program.java

---
+++ generic program.java
@@ -0,0 +1,33 @@
+//1.Write a java generic program to create class that takes one type parameter to print the different data type value and its class name.
+//CODE:
+import java.util.*;
+class Test<T>
+{
+T obj;
+Test(T obj)
+{
+this.obj=obj;
+}
+public T getObject()
+{
+return this.obj;
+}
+public void print()
+{
+System.out.println(obj.getClass().getName());
+}
+}
+public class gen01 {
+public static void main(String[] args)
+{
+Test <Integer> Obj1=new Test<Integer>(15);
+System.out.println(Obj1.getObject());
+Obj1.print();
+Test <String> Obj2=new Test<String>("JAVA");
+System.out.println(Obj2.getObject());
+Obj2.print();
+Test <Double> Obj3=new Test<Double>(19.6);
+System.out.println(Obj3.getObject());
+Obj3.print();
+}
+}
