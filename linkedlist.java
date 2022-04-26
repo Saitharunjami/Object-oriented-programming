2b5389a69eb7c9648995830e5f2f1b59390c1965
Jami Sai Tharun <saitharunjami@gmail.com>
2022-04-26 12:22:08 +0530

    Create linkedlist.java

---
+++ linkedlist.java
@@ -0,0 +1,30 @@
+
+//1)Write a program that implements the working of a Java LinkedList and perform some operations like - Add elements to the list – Access elements from the list – Remove an element from the list. 
+
+//CODE:
+
+import java.util.LinkedList;
+import java.util.Queue;
+ class Q1 {
+public static void main(String[] args)
+{
+Queue<Integer> q = new LinkedList<>();
+for (int i = 1; i < 8; i++)
+        q.add(i);
+            // Display contents of the queue.
+       System.out.println("Elements of queue "+ q);
+            // To remove the head of queue.
+       int removedele = q.remove();
+       System.out.println("removed element-" + removedele);
+       System.out.println(q);
+             // To view the head of queue
+       int head = q.peek();
+       System.out.println("head of queue-" + head);
+             // Rest all methods of collection
+            // interface like size and contains
+            // can be used with this
+           // implementation.
+     int size = q.size();
+     System.out.println("Size of queue-" + size);
+}
+}
