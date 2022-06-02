debc7cd8f9900a5e3c8b5717eccfcedbacbc77b8
Jami Sai Tharun <saitharunjami@gmail.com>
2022-06-02 14:25:32 +0530

    Update exception.java

--- exception.java
+++ exception.java
@@ -1,5 +1,4 @@
 //3) Define a new exception, called ExceptionLineTooLong, that prints out the error message "The string is too long". Write a program that reads all user entered string message and throws an exception of type ExceptionLineTooLong in the case where a string is longer than 80 characters. Handle also all exceptions that could be thrown by the program.
-
 //CODE:
 
 import java.util.*;
