0c446935dd0f3e1964dc8c2a64ffa7d9bf532d38
Jami Sai Tharun <saitharunjami@gmail.com>
2022-06-20 14:11:38 +0530

    Update degreequestion.java

--- degreequestion.java
+++ degreequestion.java
@@ -1,5 +1,6 @@
 //1) Create a class 'Degree' having a method 'getDegree' that prints "I got a degree". It has two subclasses namely 'Undergraduate' and 'Postgraduate' each having a method with the same name that prints "I am an Undergraduate" and "I am a Postgraduate"respectively. Call the method by creating an object of each class and print the studentdetails(name,regno,degree,years)bycreatingamethod“display” in class Degree
 //CODE:
+
 import java.util.*;
 class UnderGraduate extends Degree
 {
